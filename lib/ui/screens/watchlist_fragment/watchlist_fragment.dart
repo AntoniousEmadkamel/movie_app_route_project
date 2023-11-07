872c3ebf79ebdf6a73690fda0d11bbab898b8e60
sohaelmazoon22 <92324578+sohaelmazoon22@users.noreply.github.com>
2023-11-07 21:02:53 +0200

    no movies found in watchlist

--- lib/ui/screens/watchlist_fragment/watchlist_fragment.dart
+++ lib/ui/screens/watchlist_fragment/watchlist_fragment.dart
@@ -5,57 +5,78 @@ import 'package:movie_app_route_project/ui/screens/watchlist_fragment/watchlist_
 import '../../../database_utils/database_utils.dart';
 import '../../../models/movie_model.dart';
 
-
 class WatchlistTab extends StatefulWidget {
-
-
   @override
   State<WatchlistTab> createState() => _WatchlistTabState();
 }
 
 class _WatchlistTabState extends State<WatchlistTab> {
+
   @override
   Widget build(BuildContext context) {
+    List<Movie> messages = [];
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color.fromRGBO(18, 19, 18, 1.0),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text('WatchList',style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: const Text(
+            'WatchList',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
             ),
-            Expanded(
-              child: StreamBuilder<QuerySnapshot<Movie>>(
-                stream: DatabaseUtils.readMoviesFormFirebase2(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(child: CircularProgressIndicator(
-                      color: Color.fromRGBO(255, 187, 59, 1.0),
-                    ));
-                  }
-                  List<Movie> messages =
-                      snapshot.data?.docs.map((task) => task.data()).toList() ?? [];
-                  return  ListView.separated(
-                    separatorBuilder: (context, index) => Divider(color: Colors.grey,thickness: 1,),
-                    itemCount: messages.length,
-                    itemBuilder: (context, index) {
-
-                      return WatchListItem(messages[index]);
-
-                    },
-
-                  );
-                },
-              ),
-            ),
-          ],
+          ),
         ),
+        body: messages.isEmpty
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Center(child: Image.asset('assets/iconmaterial.png')),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  const Center(
+                    child: Text(
+                      'No Movies Found',
+                      style: TextStyle(
+                        color: Color.fromRGBO(255, 255, 255, 0.6),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : Expanded(
+                child: StreamBuilder<QuerySnapshot<Movie>>(
+                  stream: DatabaseUtils.readMoviesFormFirebase2(),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return Center(
+                          child: CircularProgressIndicator(
+                        color: Color.fromRGBO(255, 187, 59, 1.0),
+                      ));
+                    }
+                    messages = snapshot.data?.docs
+                            .map((task) => task.data())
+                            .toList() ??
+                        [];
+                    setState(() {});
+                    return ListView.separated(
+                      separatorBuilder: (context, index) => Divider(
+                        color: Colors.grey,
+                        thickness: 1,
+                      ),
+                      itemCount: messages.length,
+                      itemBuilder: (context, index) {
+                        return WatchListItem(messages[index]);
+                      },
+                    );
+                  },
+                ),
+              ),
       ),
     );
   }
